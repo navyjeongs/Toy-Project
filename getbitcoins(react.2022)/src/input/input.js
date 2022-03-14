5d25f9c57b7b6fb7dec37d4e20ebfe209f6b42fc
Jeong Seok <wjdtjr8649@naver.com>
2022-03-14 19:37:32 +0900

    2nd commit

--- getbitcoins(react.2022)/src/input/input.js
+++ getbitcoins(react.2022)/src/input/input.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import "./input.css"
+import ChangeMoneyToCoin from "../ChangeMoneyToCoin";
+
 
 export default function Input() {
+
+    const [money, getMoney] = useState(0);
+
+    const getMoneyfunc = (e) => {
+        e.preventDefault();
+        console.log(e);
+        getMoney(e.target[0].value);
+    }
+
     return(
-        <form className="coinInput">
-        <input className="moneyInput"  type="number" placeholder='Write the money you have ($ USD)'></input>
-        <button type='submit'>Submit</button>
-        </form>
+        <>
+            <form className="coinInput" onSubmit={getMoneyfunc}>
+                <input
+                className="moneyInput" type="number" placeholder='Write the money you have ($ USD)'></input>
+                <input type="submit" value="Click" ></input>
+                <div>
+                 {money !==0 ? <h2>You have ${money} USD </h2>: null}
+                 <ChangeMoneyToCoin money={money}/>
+                </div>
+            </form>
+        </>
     );
 }
